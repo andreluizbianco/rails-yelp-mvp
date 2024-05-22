19dbb6b0d86ef17c40577c3ab1efac727d237e96
Andre <andre.luiz.bianco@gmail.com>
2024-05-22 17:25:48 +0100

    exer

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,19 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: "chinese" }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: "belgian" }
+picapau = { name: 'picapau', address: 'sitio', category: "italian" }
+cascudo = { name: 'cascudo ', address: 'rua da abelha', category: "french" }
+peixe = { name: 'peixe', address: 'rua da coisa', category: "chinese" }
+
+[dishoom, pizza_east, picapau, cascudo, peixe].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
